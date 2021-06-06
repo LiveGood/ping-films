3091ff889109a485023b9f053d04b2c20a63013e
Deyan Dachev <fleshthps@gmail.com>
2021-06-06 20:19:00 +0300

    Added autoplay

--- components/video.js
+++ components/video.js
@@ -4,7 +4,7 @@ import styles from '../styles/video.module.css'
 export const BackgroundVideo = () => {
   return (
     <div className={styles.videoContainer} >
-      <video src={require('../public/videos/video-ping.mp4')} className={styles.video} loop muted />
+      <video src={require('../public/videos/video-ping.mp4')} className={styles.video} loop muted autoPlay />
       {/* <source type="video/mp4" /> */}
     </div>
   )
